23a61370141632374d08cd31e07512f85ab81491
Shahreen Hussain <gh6429@wayne.edu>
2020-09-19 19:49:40 -0400

    Adding Author Name

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,5 +1,6 @@
 // Simple C++ program to display "Hello World" 
 // Header file for input output functions 
+// By: Shahreen Hussain, gh6429@wayne.edu
 #include<iostream>  
   
 using namespace std; 
